a8e1f2b4c5faed473e63b14f298a275584630c5e
Pranit <pranitmalhotra1@gmail.com>
2025-12-06 22:05:57 +0530

    added more sounds

--- frontend/src/sounds/ChessSounds.ts
+++ frontend/src/sounds/ChessSounds.ts
@@ -1,10 +1,13 @@
+import { Castle } from "@mui/icons-material";
+
 export const ChessSounds = {
   MOVE: new Audio("/sounds/move.mp3"),
   CAPTURE: new Audio("/sounds/capture.mp3"),
   CHECK: new Audio("/sounds/check.mp3"),
+  CASTLE: new Audio("/sounds/castle.mp3"),
   CHECKMATE: new Audio("/sounds/checkmate.mp3"),
-  GAME_START: new Audio("/sounds/game_start.mp3"),
-  GAME_END: new Audio("/sounds/game_end.mp3"),
+  GAME_START: new Audio("/sounds/gameStart.mp3"),
+  GAME_END: new Audio("/sounds/gameEnd.mp3"),
 };
 
 Object.values(ChessSounds).forEach(sound => sound.load());
